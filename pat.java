b6c43afe25da2a251ed9bf0d3daad97620a9efbf
purva5499 <gobuddy5499@gmail.com>
2023-03-08 15:28:03 +0530

    Add program

---
+++ pat.java
@@ -0,0 +1,13 @@
+public class pat
+{
+	public static void main(String args[])
+	{
+		int i,j,r=5;
+		for(i=0;i<r;i++){
+			for(j=0;j<=i;j++){
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
